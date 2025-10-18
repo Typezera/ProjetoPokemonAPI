8ea1bea0b2a23bc43f1d4653b5b519a99f84597b
Typezera <willianbardela2131@hotmail.com>
2025-10-18 16:38:52 -0300

    feat: add @jwt_required in controllers

--- controller/pokemons/pokemon_controller.py
+++ controller/pokemons/pokemon_controller.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify, request
+from middleware.auth_middleware import jwt_required
 from services.consumer.pokemon_service import PokemonService
 
 pokemon_bp = Blueprint('pokemon_bp', __name__)
 
 @pokemon_bp.route('/pokemons', methods=['GET'])
+@jwt_required
 def get_pokemons():
     limit = request.args.get("limit", default=50, type=int)
     offset = request.args.get("offset", default=0, type=int)
@@ -11,6 +13,7 @@ def get_pokemons():
     return jsonify(response), status
 
 @pokemon_bp.route('/pokemons/<name>', methods=['GET'])
+@jwt_required
 def get_pokemon_by_name(name):
     response, status = PokemonService.get_pokemon_by_name(name)
     return jsonify(response), status
